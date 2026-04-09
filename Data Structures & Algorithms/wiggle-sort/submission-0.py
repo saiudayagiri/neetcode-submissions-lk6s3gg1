03caf1b55cbc801b6bb953b2c616a4e190b5160b
Sai Udayagiri <saibabu.udayagiri@gmail.com>
2026-04-09 16:48:24 +0530

    Add: wiggle-sort - submission-0

---
+++ Data Structures & Algorithms/wiggle-sort/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        nums.sort()
+        for i in range(1,len(nums)-1,2):
+            nums[i],nums[i+1]=nums[i+1],nums[i]
+        return nums
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
