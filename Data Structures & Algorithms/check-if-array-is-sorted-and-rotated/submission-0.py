e164b98bc4fa14b5f82793c1bb47a2a06524eb5c
Sai Udayagiri <saibabu.udayagiri@gmail.com>
2026-04-14 12:00:49 +0530

    Add: check-if-array-is-sorted-and-rotated - submission-0

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        l=0
+        n=len(nums)
+        for i in range(1,2*len(nums)):
+            if nums[i%n]<nums[(i-1)%n]:
+                l=i
+            if i-l+1==n:
+                return True
+        return False
+        
