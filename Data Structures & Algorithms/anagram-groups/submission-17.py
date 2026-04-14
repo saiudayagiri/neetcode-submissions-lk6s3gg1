77b31411757774655653588580b038d3a9a124be
Sai Udayagiri <saibabu.udayagiri@gmail.com>
2026-04-14 16:36:30 +0530

    Add: anagram-groups - submission-17

---
+++ Data Structures & Algorithms/anagram-groups/submission-17.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for string in strs:
+            res[tuple(sorted(string))].append(string)
+        return list(res.values())
+
+
+
+        
