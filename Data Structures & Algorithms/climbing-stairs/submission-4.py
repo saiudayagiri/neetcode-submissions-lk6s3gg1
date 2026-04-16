7f9bda47fea301f3e1c6238d10cdbe28ad11c7db
Sai Udayagiri <saibabu.udayagiri@gmail.com>
2026-04-16 16:52:27 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=2:
+            return n
+        arr = [0] * (n+1)
+        arr[1]=1
+        arr[2]=2
+        for i in range(3,n+1):
+            arr[i]=arr[i-1]+arr[i-2]
+        return arr[n]
+        
