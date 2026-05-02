60c548c1bfa410a86413541d8388e7fd902ae6ae
Sai Udayagiri <saibabu.udayagiri@gmail.com>
2026-05-02 22:03:23 +0530

    Add: buy-two-chocolates - submission-4

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        mini1 = float("inf")
+        mini2 = float("inf")
+        for price in prices:
+            if price < mini1:
+                mini2 = mini1
+                mini1 = price
+            elif price < mini2:
+                mini2 = price
+        if mini1 + mini2 > money:
+            return money
+        return money - mini1 - mini2        
